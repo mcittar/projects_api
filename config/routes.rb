0ab53008ee52c8d52ba67c2d7095721fdc4aa473
Michael Cittar <mcittar@gmail.com>
2017-06-12 18:49:50 -0700

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :createproject, only: [:create]
+  get "requestproject" => "requestproject#show"
 end
