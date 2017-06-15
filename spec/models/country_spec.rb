ec38a2dc6d6aeac5f487295704ccadf4ca2c463e
Michael Cittar <mcittar@gmail.com>
2017-06-14 17:49:07 -0700

    Finish Country tests

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Country, type: :model do
   let(:incomplete_country) { Country.new }
-  
-  it "validates presence of name" do
-    expect(incomplete_country).not_to be_valid
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+
+    it "validates presence of name" do
+      expect(incomplete_country).not_to be_valid
+    end
   end
+
+  describe "associations" do
+    it { should have_many(:projects).through(:target_countries) }
+  end
+
 end
